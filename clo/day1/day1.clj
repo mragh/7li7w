d01a090bc6f33011a2e67f06e890c95c1394f713
Jake Kreider <jrkreid@gmail.com>
2015-06-21 21:28:37 -0500

    Picking back up, Day 1 Clojure

---
+++ clo/day1/day1.clj
@@ -0,0 +1,11 @@
+(defn big [st n] (> (count st) n))
+(big "blah" 3)
+(big "blah" 4) 
+
+
+(defn collection-type [col] (
+		  def collections-available {(type {}) :map, (type (list 1)) :list, (type []) :vector})
+		  (collections-available (type col)))
+(collection-type [1,2,3])
+(collection-type {:a "a"})
+(collection-type (list 1 2 3))
